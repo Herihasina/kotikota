99075694e309f0cabed42990ea086411dcf2a751
Hasina <herihasina.rr@gmail.com>
2021-10-20 11:47:44 +0300

    .

--- page-participer_cagnotte.php
+++ page-participer_cagnotte.php
@@ -254,7 +254,7 @@
                 <div class="blc-chp acc">
                   <label><input type="checkbox" name="accord" id="accord">
                     <?php
-                      printf( __('En participant à la cagnotte vous acceptez les %1s et la %2s','kotikota'), '<a href="'.get_site_url( 'cgu' ).'" title="">CGU</a>', '<a href="'. get_site_url('politique-de-confidentialite') .'">'.__('politique de confidentialité','kotikota').'</a>' )
+                      printf( __('En participant à la cagnotte vous acceptez les %1s et la %2s','kotikota'), '<a href="'.home_url('/cgu/').'" title="">CGU</a>', '<a href="'. get_site_url('politique-de-confidentialite') .'">'.__('politique de confidentialité','kotikota').'</a>' )
                     ?>
                   </label>
                 </div>
