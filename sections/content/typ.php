43c0945eb416b6e4d533775e6763ed7b2d90acb1
Micky <faellafdr@gmail.com>
2021-12-20 11:37:35 +0300

    felicitation

--- sections/content/typ.php
+++ sections/content/typ.php
@@ -2,13 +2,13 @@
   <div class="wrapper">
         <div class="fom-participe">
             <div class="titre wow fadeIn" data-wow-delay="800ms">
-               <h2><?= __('Féliciations et Merci !','kotikota') ?></h2>
+               <h2><?= __('Félicitations','kotikota') ?></h2>
             </div>
             <div class="blc-liste-cagnote resultat-recherche wow fadeIn" data-wow-delay="850ms">
                 <div class="lst-cagnotte-publique wow fadeIn clr" data-wow-delay="900ms">
                     <div style="text-align:center">
                       <p>
-                        <?= __('Votre don a bien été effectuée.','kotikota') ?>
+                        <?= __('Votre don a bien été effectué.','kotikota') ?>
                       </p>
                       <p>
                         <a href="<?= site_url('toutes-les-cagnottes') ?>" title="cagnottes">
