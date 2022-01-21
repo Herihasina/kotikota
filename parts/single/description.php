69b283c8ce6202003afc6de55cd36f2d1b781740
Hasina <herihasina.rr@gmail.com>
2022-01-21 09:18:19 +0300

    .

--- parts/single/description.php
+++ parts/single/description.php
@@ -16,14 +16,14 @@
   <?php if ( get_field('description_de_la_cagnote') ) : ?>
     <p>
       <?php 
-        if ( 
-          $type_cagnotte == "solidaire" || 
-          $isInvited ||
-          get_field('titulaire_de_la_cagnotte')  == get_current_user_id() || 
-          current_user_can('administrator') 
-        ){
+        // if ( 
+        //   $type_cagnotte == "solidaire" || 
+        //   $isInvited ||
+        //   get_field('titulaire_de_la_cagnotte')  == get_current_user_id() || 
+        //   current_user_can('administrator') 
+        // ){
           echo get_field('description_de_la_cagnote');
-        }
+        // }
         // elseif ( $type_cagnotte == "perso"){
         //   echo substr(get_field('description_de_la_cagnote'), 0, 220 ).'...';
         // }
