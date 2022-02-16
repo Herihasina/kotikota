f5977e35b47280e78b1ef49f774f648394c852de
Angela <angela.rakotomalala@gmail.com>
2022-02-16 14:55:33 +0300

    Tâche n°1366

--- parts/single/description.php
+++ parts/single/description.php
@@ -30,11 +30,52 @@
       ?>
     </p>
   <?php endif; ?>
-</div> 
-<div class="pp-document">
-  <div class="blcbtn">
-    <a href="#" class="link" title="Document">Document</a>
-    <a href="#" class="link" title="Photos et vidéos">Photos et vidéos</a>
 
+  <div class="pp-document-photos">
+    <div class="blcbtn">
+      <a href="#pp-document" class="link fancybox" title="Document">Document</a>
+      <a href="#" class="link" title="Photos et vidéos">Photos et vidéos</a>
+
+      <div class="pp-document" id="pp-document" style="display: none">
+        <div class="Document cont-pp">
+            <div class="titre">
+                <h2>Documents</h2>
+            </div>
+            <div class="inner-pp">
+              <div class="scroll-wrapper lst-document scrollbar-inner">
+                  <div class="row">
+                    <div class="col">
+                      <h3>documents word</h3>
+                      <div class="lst-option">
+                        
+                        <div class="item">
+                          <input type="checkbox" class="document" id="doc1"> 
+                          <label for="doc1">
+                            <div class="ico"><img src="<?= IMG_URL ?>word.png" alt="Kotikota"></div>
+                            <div class="txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do</div>
+                          </label>
+                        </div>
+
+                        <div class="item">
+                          <input type="checkbox" class="document" id="doc2"> 
+                          <label for="doc2">
+                            <div class="ico"><img src="<?= IMG_URL ?>word.png" alt="Kotikota"></div>
+                            <div class="txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do</div>
+                          </label>
+                        </div>
+
+
+                      </div>
+                    </div>
+                    <div class="col">
+                      <h3>documents pdf</h3>
+                    </div>
+                  </div>
+              </div>
+            </div>
+        </div>
+      </div>
+
+    </div>
   </div>
-</div>
+</div> 
