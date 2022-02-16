fdd312b0cbb8c16d32426617606121b0e95d7b49
Angela <angela.rakotomalala@gmail.com>
2022-02-16 18:37:03 +0300

    Tâche n°1366

--- parts/single/description.php
+++ parts/single/description.php
@@ -229,12 +229,15 @@
                             
                           </label>
                         </div>
+
                         <div class="item">
                           <input type="checkbox" class="ck-photo" id="img8"> 
                           <label for="img8">
-                            <a href="#" class="img"><img src="<?= IMG_URL ?>photo8.jpg" alt="Kotikota"></a>    
+                            <a href="#" class="img"><img src="<?= IMG_URL ?>photo8.jpg" alt="Kotikota"></a>          
                           </label>
                         </div>
+
+                      
 
                       </div>
                     </div>
