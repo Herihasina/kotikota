36c06febbbd3a31633778eb3a4cdab427e0ada04
Angela <angela.rakotomalala@gmail.com>
2022-02-16 10:19:42 +0300

    Tâche n°1369

--- sections/content/qui-sommes-nous.php
+++ sections/content/qui-sommes-nous.php
@@ -2,7 +2,7 @@
 
 <div class="qui-sommes-nous" id="qui-sommes-nous" >
     <div class="wrapper">
-        
+        <h2 class="wow fadeInUp" data-wow-delay="900ms" style="visibility: visible; animation-delay: 900ms; animation-name: fadeInUp;">Qui sommes-nous ?</h2>
      
     </div>
 </div>
