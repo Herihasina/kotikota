c14d755dddc122629f5a4dafc9bec79ab7f98bdc
Angela <angela.rakotomalala@gmail.com>
2022-02-18 17:40:27 +0300

    Tâche n°1374

--- parts/single/image.php
+++ parts/single/image.php
@@ -1,5 +1,5 @@
 <div class="blc-img">
-    <?php 
+   <!--  <?php 
       $img_url = attachment_url_to_postid(get_field('illustration_pour_la_cagnotte'));
       if ( $img_url )
         echo wp_get_attachment_image( $img_url, 'banniere-single' );
@@ -19,7 +19,7 @@
             <img src="<?php echo IMG_URL ?>ok.png">
           </span>
     <?php 
-        }elseif ( $statu < 100 && $limited ){ ?>          
+        }elseif ( $statu < 100 && $limited ){ ?>    -->       
          <!--  <span class="ico-percent">
               <div class="percent" style="width:45px;height:45px;">
                 <p style="display:none;"><?php echo $statu ?>%</p>
@@ -28,7 +28,7 @@
          <div class="percent" style="width:50px;height:50px;">
             <p style="display:none;">75%</p>
           </div>
-    <?php } ?>
+ <!--    <?php } ?>
     <?php if( $closed ): ?>
         <span class="ico-percent cloturer">
           <div class="ckeckCloturer"></div>
@@ -36,6 +36,6 @@
         <span class="blocCloture">
           <?= __('Cette cagnotte est clôturée','kotikota') ?>
         </span>
-    <?php endif; ?></div>
+    <?php endif; ?></div> -->
 
 
