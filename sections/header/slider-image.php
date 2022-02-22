88debbe2536fc60093f986a5afb8247690737466
Angela <angela.rakotomalala@gmail.com>
2022-02-22 18:20:12 +0300

    Tâche n°1362

--- sections/header/slider-image.php
+++ sections/header/slider-image.php
@@ -43,7 +43,7 @@
                             <div class="pp-video" id="pp-video">
                                 <div class="content">
                                      <div class="modal-body">
-                                        <iframe width="870" height="489" src="https://www.youtube.com/embed/SvuoDerqNW0" frameborder="0" allowfullscreen=""></iframe></div>
+                                        <iframe width="870" height="489" src="https://www.youtube.com/watch?v=H1DUszPZakQ&t=4s" frameborder="0" allowfullscreen=""></iframe></div>
                                 </div>
                             </div>
                         </div>   
