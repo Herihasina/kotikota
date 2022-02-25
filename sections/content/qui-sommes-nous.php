58cac74fd230dc2590faffc646f23f6ce384217a
onyrf82 <74725872+onyrf82@users.noreply.github.com>
2022-02-25 13:26:20 +0300

    Contenu dynamique

--- sections/content/qui-sommes-nous.php
+++ sections/content/qui-sommes-nous.php
@@ -2,22 +2,25 @@
 
 <div class="qui-sommes-nous" id="qui-sommes-nous" >
     <div class="wrapper">
-        <h2 class="wow fadeInUp" data-wow-delay="900ms" style="visibility: visible; animation-delay: 900ms; animation-name: fadeInUp;">Qui sommes-nous ?</h2>
+        <h2 class="wow fadeInUp" data-wow-delay="900ms" style="visibility: visible; animation-delay: 900ms; animation-name: fadeInUp;"><?php the_sub_field('qui_sommes_nous_titre') ?></h2>
         <div class="txt">
-            <p>
-Fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. </p>
+            <p><?php the_sub_field('qui_sommes_nous_texte') ?></p>
         </div>
         <div class="slide-qui-sommes-nous" id="slide-qsn">
+            <?php
+                $slide_images = get_field('qui_sommes_nous_images');
+
+                if ($slide_images) :                
+                if( is_array( $slide_images )):
+                foreach ( $slide_images as $slide_image ){
+            ?>
             <div class="item">
                 <div class="img">
-                    <img src="<?= IMG_URL ?>img-qsn.jpg" alt="Kotikota">
+                    <img src="<?= $slide_image['qui_sommes_nous_image'] ?>" alt="Kotikota">
                 </div>
-            </div>
-            <div class="item">
-                <div class="img">
-                    <img src="<?= IMG_URL ?>img-qsn.jpg" alt="Kotikota">
-                </div>
-            </div>
+            </div>            
+            <?php } ?>
+            <?php endif; endif; ?>
         </div>
      
     </div>
