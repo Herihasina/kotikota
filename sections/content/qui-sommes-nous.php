64f75d07ffb32b49a578663d7d2f0a6992e645fc
Angela <angela.rakotomalala@gmail.com>
2022-03-04 10:19:10 +0300

    Encrage menu

--- sections/content/qui-sommes-nous.php
+++ sections/content/qui-sommes-nous.php
@@ -1,5 +1,5 @@
 
-<div class="qui-sommes-nous" id="qui-sommes-nous" >
+<div class="qui-sommes-nous" id="comment-ca-marche" >
     <div class="wrapper">
         <h2 ><?= get_field('qui_sommes_nous_titre') ?></h2>
         <div class="txt">
