c76e07a85fc10fa25ca9babdf6622847ac2e34df
Anna Razafindramiarina <anna@maki-agency.com>
2022-03-07 15:04:58 +0300

    Mon compte : Afficher l’historique de nos participations aux cagnottes #1368 => ajout nouveau template

---
+++ page-lister_mes_participations.php
@@ -0,0 +1,61 @@
+<?php 
+	/*
+	Template Name: Liste mes participations
+	*/
+	// global $wp_query;
+
+	get_header();
+?>
+<main id="homepage">
+<?php
+	include 'sections/content/parallax.php';
+?>
+
+<div class="blc-cagnotte-liste wow fadeIn" data-wow-delay="850ms">
+    <div class="wrapper">
+        <?php
+        	$per_page = get_field('per_page','options');
+        	$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+        	$args = array(  
+            'post_type' => array('cagnotte','cagnotte-perso'),
+            'post_status' => 'publish',
+            'posts_per_page' => $per_page, 
+            'orderby' => 'ID',
+            'order' => 'DESC',
+            'paged' => $paged,
+            'meta_query' => array(
+                array(
+                    'key' => 'titulaire_de_la_cagnotte',
+                    'value' => get_current_user_id()
+
+                )
+            )
+        );
+
+        $loop = query_posts( $args );
+        
+        if (have_posts()):
+            include 'sections/content/liste-cagnottes.php';
+        else:
+    ?>
+            <div class="blc-liste-cagnote wow fadeIn" data-wow-delay="850ms">
+                <div class="lst-cagnotte-publique wow fadeIn clr" data-wow-delay="900ms">
+                    <div style="text-align:center">
+                        <h3 style="text-align:center">
+                            <?php printf( __( 'Aucune cagnotte n\'a été trouvée', 'kotikota' ), esc_html( get_search_query() ) ); ?>
+                        </h3>
+                    </div>
+                </div>
+            </div>
+    <?php            
+        endif;
+    ?>
+      	
+    </div>
+ </div>
+ <div id="loader">
+  <img src="<?php echo IMG_URL.'loader.gif' ?>" alt="loader">
+</div>
+</main>
+<?php get_footer(); ?>
