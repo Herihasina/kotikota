7e5d507eb232d71ba12f4195a93e4b938b3d5b3e
onyrf82 <74725872+onyrf82@users.noreply.github.com>
2022-03-07 09:58:27 +0300

    dynamisation

--- sections/content/securite-cagnotte.php
+++ sections/content/securite-cagnotte.php
@@ -2,27 +2,21 @@
 
 <div class="securite-cagnotte">
     <div class="wrapper">
-        <h2 class="wow fadeInUp" data-wow-delay="900ms" style="visibility: visible; animation-delay: 900ms; animation-name: fadeInUp;">Sécurité des cagnottes</h2>
+        <h2 class="wow fadeInUp" data-wow-delay="900ms" style="visibility: visible; animation-delay: 900ms; animation-name: fadeInUp;"><?= get_field('securite_cacgnotte_titre') ?></h2>
         <div class="lst-securite">
-            <div class="item">
-                <div class="titre">Une équipe vouée pour la lutte contre la fraude et le blanchiment d’argent</div>
-                <div class="txt"><p>Notre équipe conformité relève la responsabilité du contrôle des cagnottes. Ce service est chargé d'assurer la fiabilité, la sécurité des utilisateurs et l'interaction des activités de collecte de fonds créées sur Koti Kota. Vérifiez régulièrement pour vous assurer que les fonds ont été envoyés aux bénéficiaires visés.</p></div>
-            </div>
-            <div class="item">
-                <div class="titre">Vous êtes autorisés à signaler une cagnotte pour que notre équipe l’examine</div>
-                <div class="txt">
-                    <p>Pour cela, il vous suffit d’envoyer un email à cette adresse hello@koti-kota.com. Indiquez les détails de la cagnotte en question et décrivez ensuite la raison pour laquelle vous signalez cette cagnotte.</p>
+            <?php
+                $list_securite = get_field('securite_cacgnotte_liste');
+                if($list_securite):
+                foreach ($list_securite as $key => $securite) {
+            ?>
+                <div class="item">
+                    <div class="titre"><?= $securite['securite_cacgnotte_liste_titre'] ?></div>
+                    <div class="txt"><p><?= $securite['securite_cacgnotte_liste_texte'] ?></p></div>
                 </div>
-            </div>
-            <div class="item">
-                <div class="titre">En cas de fraude, nous sommes là pour vous</div>
-                <div class="txt">
-                    <p>Koti Kota est en contact avec des autorités compétentes. Si votre carte bancaire ou votre mobile money est utilisée frauduleusement sur notre site , notre équipe vous conseillera et accompagnera dans votre démarche.</p>
-                </div>
-            </div>
+            <?php
+                endif;
+                }
+            ?>
         </div>
-       
-  
-     
     </div>
 </div>
