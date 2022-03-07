e757f36c5d220ea622658f15404bd468c84259c9
onyrf82 <74725872+onyrf82@users.noreply.github.com>
2022-03-07 14:40:41 +0300

    popup création cagnotte

--- sections/content/creer-cagnotte.php
+++ sections/content/creer-cagnotte.php
@@ -257,12 +257,13 @@
                         <div class="btn">
                             <a href="<?php echo home_url() ?>" class="link" title="<?php _e('annuler','kotikota'); ?>"><?php _e('annuler','kotikota'); ?></a> 
                             <input type="submit" class="link submit" value="<?php _e('créer ma cagnotte','kotikota'); ?>" id="creer-cagnotte">
-                            <a href="#pp-felicitation" class="link submit fancybox" id="creer-cagnotte">créer ma cagnotte pop up</a>
+                            <a href="#pp-felicitation" style="display: none" class="link submit fancybox" id="creer-cagnotte-popup">créer ma cagnotte pop up</a>
                         </div>
                         <div class="pp-felicitation" id="pp-felicitation" style="display: none">
                             <div class="content">
+                                <?php $current_user  = wp_get_current_user(); ?>
                                 <div class="conf_titre">Félicitations</div>
-                                <div class="conf_text"><p>Vous avez reçu un email à l'adresse dianarasolofoarinirina@gmail.com afin que vous puissiez confirmer votre compte . Attention, il se peut que l'email tombe dans les courriers indésirables (spam). </p> </div>
+                                <div class="conf_text"><p>Vous avez reçu un email à l'adresse <span class="email"><?= $current_user->user_email ?></span> afin que vous puissiez confirmer votre compte . Attention, il se peut que l'email tombe dans les courriers indésirables (spam). </p> </div>
                                 <div class="btn">
                                   <a href="#" id="Ok" class="link" title="Ok"> ok </a>
                                 </div>
