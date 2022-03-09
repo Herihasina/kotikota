8c1188b9d9c1cb8510116fbdc466508d2b544cce
onyrf82 <74725872+onyrf82@users.noreply.github.com>
2022-03-09 15:29:23 +0300

    debug php

--- sections/content/comment-ca-marche.php
+++ sections/content/comment-ca-marche.php
@@ -16,14 +16,14 @@
                             )
                         );
 
-                        /*$q = new WP_Query( $args );
+                        $q = new WP_Query( $args );
                         while( $q->have_posts() ) {
-                                $q->the_post();*/
+                                $q->the_post();
                                 //$ca_id = get_the_ID();
                                 $url_invite = 'https://koti-kota.com/gestion-cagnotte-invite/?gest=';
                                 $url_class_invite = '';
-                        /*}
-                        wp_reset_postdata();*/
+                        }
+                        wp_reset_postdata();
                     } else {
                         $url_invite = '#connecter';
                         $url_class_invite = 'class="link fancybox-home"';
