2ca7a27235ee3865ac7919e1ee6f8c4737ec7e9a
onyrf82 <74725872+onyrf82@users.noreply.github.com>
2022-03-11 19:35:50 +0300

    body pdf

--- functions.php
+++ functions.php
@@ -945,10 +945,10 @@ function generate_post_to_pdf_file($postID) {
       $pdf->AddPage();
       $html .= "<body>";
       $html .= "<h2 style=\"text-align:center\">".apply_filters ( 'the_post_title', $pdf_title )."</h2><hr>";
-      $html .= "Titulaire du compte : " . get_field('rib_nom', $post->ID) .'<br>';
-      $html .= "Nom de la banque : " .get_field('rib_banque', $post->ID).'<br>';
-      $html .= "Adresse de domiciliation : " .get_field('rib_adresse_de_domiciliation', $post->ID.'<br>');
-      $html .= "RIB : " .get_field('rib_code_banque', $post->ID) .' '.get_field('rib_code_agence', $post->ID) .' '.get_field('rib_num_de_compte', $post->ID) .' '.get_field('rib_cle_rib', $post->ID).'<br>';
+      $html .= '<br>'."Titulaire du compte : " . get_field('rib_nom', $post->ID) .'<br>';
+      $html .= '<br>'."Nom de la banque : " .get_field('rib_banque', $post->ID).'<br>';
+      $html .= '<br>'."Adresse de domiciliation : " .get_field('rib_adresse_de_domiciliation', $post->ID).'<br>';
+      $html .= '<br>'."RIB : " .get_field('rib_code_banque', $post->ID) .' '.get_field('rib_code_agence', $post->ID) .' '.get_field('rib_num_de_compte', $post->ID) .' '.get_field('rib_cle_rib', $post->ID).'<br>';
       $html .="</body>";
   
       /*$html = '<h1>Welcome to <a href="http://techbriefers.com/" style="text-decoration:none;padding: 10px;"> <span style="background-color:#ef3e47;color:#fff;"> Tech</span><span style="background-color:#fff1f0;color:#000;">Briefers</span> </a>!</h1>
