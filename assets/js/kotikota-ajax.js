b173d31c2452aa2904260d623407dc67bf922578
Anna Razafindramiarina <anna@maki-agency.com>
2022-03-11 15:14:26 +0300

    Page détail cagnotte : Ajouter bouton UPLOAD FICHIER + VIDEO => debug ajax suppression fichier

--- assets/js/kotikota-ajax.js
+++ assets/js/kotikota-ajax.js
@@ -847,7 +847,8 @@ $(function(){
 	$('#remove_doc_btn').click(function(e){
 		e.preventDefault();
 		var cagnotte_id= $(this).data('cagnotteId');
-		var file_ids = $('.document-check').val();
+		var file_ids = $('[name=doc_files[]]').val();
+		console.log(file_ids);
 		$.ajax({
 			url: ajaxurl,
 			data: {
