9d2a76881f7c1a90e719ec704394ffa12fcf5a35
onyrf82 <74725872+onyrf82@users.noreply.github.com>
2022-03-11 19:51:54 +0300

    $pdf_title

--- functions.php
+++ functions.php
@@ -939,13 +939,13 @@ function generate_post_to_pdf_file($postID) {
       $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
       $pdf->SetCreator ( 'kotikota' . PDF_CREATOR );
       $pdf->SetAuthor ( get_bloginfo ( 'name' ) );
-      $pdf_title = 'Informations RIB de la cagnotte <<' . $post->post_title .'>>';
+      $pdf_title = 'Informations RIB de la cagnotte ' . $post->post_title;
   
       $pdf->setPrintHeader(false);
       $pdf->AddPage();
       $html .= "<body>";
       $html .= "<h2 style=\"text-align:center\">".apply_filters ( 'the_post_title', $pdf_title )."</h2><hr>";
-      $html .= '<br>'."Titulaire du compte : " . get_field('rib_nom', $post->ID) .'<br>';
+      $html .= '<br><br>'."Titulaire du compte : " . get_field('rib_nom', $post->ID) .'<br>';
       $html .= '<br>'."Nom de la banque : " .get_field('rib_banque', $post->ID).'<br>';
       $html .= '<br>'."Adresse de domiciliation : " .get_field('rib_adresse_de_domiciliation', $post->ID).'<br>';
       $html .= '<br>'."RIB : " .get_field('rib_code_banque', $post->ID) .' '.get_field('rib_code_agence', $post->ID) .' '.get_field('rib_num_de_compte', $post->ID) .' '.get_field('rib_cle_rib', $post->ID).'<br>';
