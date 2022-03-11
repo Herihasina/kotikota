67faf42f2016732acd345e6523fe62be1114a111
onyrf82 <74725872+onyrf82@users.noreply.github.com>
2022-03-11 14:41:24 +0300

    rib_pdf_admin_enqueue_scripts

--- functions.php
+++ functions.php
@@ -892,7 +892,7 @@ add_action('admin_enqueue_scripts', 'rib_pdf_admin_enqueue_scripts');
 function rib_pdf_admin_enqueue_scripts() {
     wp_enqueue_script( 'rib-pdf-input-js', get_stylesheet_directory_uri() . '/assets/js/admin-script.js', false, '1.0.0' );
 
-    wp_localize_script( 'admin-script-js', 'ajax_object', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
+    wp_localize_script( 'rib-pdf-input-js', 'ajax_object', array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
 }
 
 add_action('wp_ajax_download_rib_report', 'download_rib_report_handler');
