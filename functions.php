54a9fa587d9a7abf7142f0943d67c1d87d553475
Anna Razafindramiarina <anna@maki-agency.com>
2022-03-11 17:00:59 +0300

    Page détail cagnotte : Ajouter bouton UPLOAD FICHIER + VIDEO => ajout video gestion erreur id video

--- functions.php
+++ functions.php
@@ -865,7 +865,7 @@ function get_youtube_video_detail($video_id){
 
   $youtubeVals = json_decode(json_encode($youtubeData), true);
 
-  if($youtubeVals['pageInfo']['totalResults']>0){
+  // if($youtubeVals['pageInfo']['totalResults']>0){
     $video_data['url'] = "https://www.youtube.com/watch?v=".$video_id;
     $video_data['title'] = $youtubeVals['items'][0]['snippet']['title'];
     $video_data['description'] = wp_trim_words( $youtubeVals ['items'][0]['snippet']['description'], 6, '...' );
@@ -874,9 +874,9 @@ function get_youtube_video_detail($video_id){
     $date_interval= new DateInterval($iso8601_duration);
     $video_data['duration']= $date_interval->i.":".$date_interval->s;
     return $video_data;
-  }else{
-    return false;
-  }
+  // }else{
+  //   return false;
+  // }
 }
 
 function custom_js_to_head() {
