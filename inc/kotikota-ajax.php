83e3fbf1dccb6fc27c3b1fd7835288f280e3e5d5
onyrf82 <74725872+onyrf82@users.noreply.github.com>
2022-03-14 10:39:15 +0300

    save_info_banque

--- inc/kotikota-ajax.php
+++ inc/kotikota-ajax.php
@@ -654,15 +654,95 @@ function save_info_banque(){
        $erreurs[] = __("Indiquer Numero BIC.", "kotikota");
     }
 
+    /*if ( $erreurs ){
+        foreach ($erreurs as $erreur ){
+             echo "<li>$erreur</li>";
+         }
+         wp_die();
+    }*/
+    
+    $idCagnotte = $_POST['idCagnotte'];
+    
+    $valid_formats = array("jpg", "png", "gif", "bmp", "jpeg"); // Supported file types
+    $max_file_size = 1024 * 500; // in kb
+    $max_image_upload = 10; // Define how many images can be uploaded to the current post
+    $wp_upload_dir = wp_upload_dir();
+    $path = $wp_upload_dir['path'] . '/';
+    $count = 0;
+
+    $attachments = get_posts( array(
+        'post_type'         => 'attachment',
+        'posts_per_page'    => -1,
+        'post_parent'       => $idCagnotte,
+        'exclude'           => get_post_thumbnail_id() // Exclude post thumbnail to the attachment count
+    ) );
+
+    if( $_SERVER['REQUEST_METHOD'] == "POST" ){
+
+        // Check if user is trying to upload more than the allowed number of images for the current post
+        if( ( count( $attachments ) + count( $_FILES['files']['name'] ) ) > $max_image_upload ) {
+            $erreurs[] = "Sorry you can only upload " . $max_image_upload . " images for each Ad";
+        } else {
+
+            foreach ( $_FILES['files']['name'] as $f => $name ) {
+                $extension = pathinfo( $name, PATHINFO_EXTENSION );
+                // Generate a randon code for each file name
+                $new_filename = cvf_td_generate_random_code( 20 )  . '.' . $extension;
+
+                if ( $_FILES['files']['error'][$f] == 4 ) {
+                    continue;
+                }
+
+                if ( $_FILES['files']['error'][$f] == 0 ) {
+                    // Check if image size is larger than the allowed file size
+                    if ( $_FILES['files']['size'][$f] > $max_file_size ) {
+                        $erreurs[] = "$name is too large!.";
+                        continue;
+
+                    // Check if the file being uploaded is in the allowed file types
+                    } elseif( ! in_array( strtolower( $extension ), $valid_formats ) ){
+                        $erreurs[] = "$name is not a valid format";
+                        continue;
+
+                    } else{
+                        // If no errors, upload the file...
+                        if( move_uploaded_file( $_FILES["files"]["tmp_name"][$f], $path.$new_filename ) ) {
+
+                            $count++;
+
+                            $filename = $path.$new_filename;
+                            $filetype = wp_check_filetype( basename( $filename ), null );
+                            $wp_upload_dir = wp_upload_dir();
+                            $attachment = array(
+                                'guid'           => $wp_upload_dir['url'] . '/' . basename( $filename ),
+                                'post_mime_type' => $filetype['type'],
+                                'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $filename ) ),
+                                'post_content'   => '',
+                                'post_status'    => 'inherit'
+                            );
+                            // Insert attachment to the database
+                            $attach_id = wp_insert_attachment( $attachment, $filename, $parent_post_id );
+
+                            require_once( ABSPATH . 'wp-admin/includes/image.php' );
+
+                            // Generate meta data
+                            $attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
+                            wp_update_attachment_metadata( $attach_id, $attach_data );
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     if ( $erreurs ){
         foreach ($erreurs as $erreur ){
              echo "<li>$erreur</li>";
          }
          wp_die();
     }
-
-    $idCagnotte = $_POST['idCagnotte'];
-
+    
     // info beneficiaire
     $idBenef   = strip_tags( $_POST['idBenef'] );
     $titulaire       = strip_tags( $_POST['titulaire'] );
