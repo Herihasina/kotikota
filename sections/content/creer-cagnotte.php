a0892a1b3da8d18f9a38ffbf0bcd9c0c614d347a
Angela <angela.rakotomalala@gmail.com>
2022-03-14 14:11:05 +0300

    cagnotte public/privé

--- sections/content/creer-cagnotte.php
+++ sections/content/creer-cagnotte.php
@@ -109,6 +109,11 @@
                                             <option value="privee"><?php _e('Cagnotte Privée','kotikota'); ?></option>
                                             <option value="publique"><?php _e('Cagnotte Publique','kotikota'); ?></option>
                                         </select>
+                                        <div class="info-cagnotte">
+                                            <div class="info-publique">Les cagnottes publiques sont visibles par tous les utilisateurs</div>
+                                            <div class="info-prive">Les cagnottes privées ne peuvent être visibles que par les personnes invitées</div>
+                                        </div>
+
                                     </div>
                                  </div>
                              </div>
