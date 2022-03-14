f527ac4616a5323e3ec54f1a07bdbfc0eb54a400
onyrf82 <74725872+onyrf82@users.noreply.github.com>
2022-03-14 11:35:36 +0300

    attachment_url_to_postid rib_file

--- inc/kotikota-ajax.php
+++ inc/kotikota-ajax.php
@@ -662,79 +662,8 @@ function save_info_banque(){
     }*/
     
     $idCagnotte = $_POST['idCagnotte'];
+    $rib_file = sanitize_url( $_POST['fichier'] );
     
-    $valid_formats = array("jpg", "png", "gif", "bmp", "jpeg"); // Supported file types
-    $max_file_size = 1024 * 500; // in kb
-    $max_image_upload = 10; // Define how many images can be uploaded to the current post
-    $wp_upload_dir = wp_upload_dir();
-    $path = $wp_upload_dir['path'] . '/';
-    $count = 0;
-
-    $attachments = get_posts( array(
-        'post_type'         => 'attachment',
-        'posts_per_page'    => -1,
-        'post_parent'       => $idCagnotte,
-        'exclude'           => get_post_thumbnail_id() // Exclude post thumbnail to the attachment count
-    ) );
-
-    if( $_SERVER['REQUEST_METHOD'] == "POST" ){
-
-        // Check if user is trying to upload more than the allowed number of images for the current post
-        if( ( count( $attachments ) + count( $_FILES['files']['name'] ) ) > $max_image_upload ) {
-            $erreurs[] = "Sorry you can only upload " . $max_image_upload . " images for each Ad";
-        } else {
-
-            foreach ( $_FILES['files']['name'] as $f => $name ) {
-                $extension = pathinfo( $name, PATHINFO_EXTENSION );
-                // Generate a randon code for each file name
-                $new_filename = cvf_td_generate_random_code( 20 )  . '.' . $extension;
-
-                if ( $_FILES['files']['error'][$f] == 4 ) {
-                    continue;
-                }
-
-                if ( $_FILES['files']['error'][$f] == 0 ) {
-                    // Check if image size is larger than the allowed file size
-                    if ( $_FILES['files']['size'][$f] > $max_file_size ) {
-                        $erreurs[] = "$name is too large!.";
-                        continue;
-
-                    // Check if the file being uploaded is in the allowed file types
-                    } elseif( ! in_array( strtolower( $extension ), $valid_formats ) ){
-                        $erreurs[] = "$name is not a valid format";
-                        continue;
-
-                    } else{
-                        // If no errors, upload the file...
-                        if( move_uploaded_file( $_FILES["files"]["tmp_name"][$f], $path.$new_filename ) ) {
-
-                            $count++;
-
-                            $filename = $path.$new_filename;
-                            $filetype = wp_check_filetype( basename( $filename ), null );
-                            $wp_upload_dir = wp_upload_dir();
-                            $attachment = array(
-                                'guid'           => $wp_upload_dir['url'] . '/' . basename( $filename ),
-                                'post_mime_type' => $filetype['type'],
-                                'post_title'     => preg_replace( '/\.[^.]+$/', '', basename( $filename ) ),
-                                'post_content'   => '',
-                                'post_status'    => 'inherit'
-                            );
-                            // Insert attachment to the database
-                            $attach_id = wp_insert_attachment( $attachment, $filename, $idCagnotte );
-
-                            require_once( ABSPATH . 'wp-admin/includes/image.php' );
-
-                            // Generate meta data
-                            $attach_data = wp_generate_attachment_metadata( $attach_id, $filename );
-                            wp_update_attachment_metadata( $attach_id, $attach_data );
-
-                        }
-                    }
-                }
-            }
-        }
-    }
 
     if ( $erreurs ){
         foreach ($erreurs as $erreur ){
@@ -756,7 +685,8 @@ function save_info_banque(){
     $bic       = strip_tags( $_POST['bic'] );    
 
     update_beneficiaire_info_rib( $idCagnotte,$titulaire,$banque,$domicile,$codebanque,$codeguichet,$numcompte,$cle,$iban,$bic );
-
+    update_field('rib_fichier', attachment_url_to_postid($rib_file), $idCagnotte );
+    
     $single = get_site_url().'/parametre-info-principale';
     echo $single;
     wp_die();
