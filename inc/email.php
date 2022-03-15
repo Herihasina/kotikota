e55e1f0052935fbdd2940cd664e6fe03f420f80c
onyrf82 <74725872+onyrf82@users.noreply.github.com>
2022-03-15 18:17:49 +0300

    supprimer email test participant création cagnotte

--- inc/email.php
+++ inc/email.php
@@ -13,13 +13,13 @@ function sendNotificationCreation($id){
 
     if( 'mg' == ICL_LANGUAGE_CODE ){
       $tpl = locate_template( 'email-tpl/creation-cagnotte.php', false, false );      
-      $tpl_participant = locate_template( 'email-tpl/notif-participation-participant.php', false, false );
+      //$tpl_participant = locate_template( 'email-tpl/notif-participation-participant.php', false, false );
     }elseif( 'en' == ICL_LANGUAGE_CODE ){
       $tpl = locate_template( 'email-tpl/creation-cagnotte.php', false, false );      
-      $tpl_participant = locate_template( 'email-tpl/notif-participation-participant.php', false, false );
+      //$tpl_participant = locate_template( 'email-tpl/notif-participation-participant.php', false, false );
     }else{
       $tpl = locate_template( 'email-tpl/creation-cagnotte.php', false, false );      
-      $tpl_participant = locate_template( 'email-tpl/notif-participation-participant.php', false, false );
+      //$tpl_participant = locate_template( 'email-tpl/notif-participation-participant.php', false, false );
     }
     
     ob_start();
@@ -29,7 +29,7 @@ function sendNotificationCreation($id){
     $objet = get_field('objet_creation','option') ? get_field('objet_creation','option') : __("Création de cagnotte avec succès","kotikota");
 
     if (@wp_mail( $email_titulaire, $objet, $html, $headers ) ){
-        $current_user = wp_get_current_user();
+        /*$current_user = wp_get_current_user();
         ob_start();
           include( $tpl_participant );
           $htmlp = ob_get_clean();
@@ -40,8 +40,8 @@ function sendNotificationCreation($id){
             return true;
           }else{
             return false;
-          }
-      //return true;
+          }*/
+      return true;
     }else{
       return false;
     }
@@ -81,11 +81,11 @@ function sendNotificationParticipation($id){
       //return true;
             ob_start();
           include( $tpl_participant );
-          $html = ob_get_clean();
+          $htmlp = ob_get_clean();
 
           $objet = get_field('objet_participation_participant','option') ? get_field('objet_participation_participant','option') : __("Participation à une cagnotte","kotikota");
 
-          if ( @wp_mail( $current_user->user_email, $objet, $html, $headers ) ){
+          if ( @wp_mail( $current_user->user_email, $objet, $htmlp, $headers ) ){
             return true;
           }else{
             return false;
