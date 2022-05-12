c12ce02db345d359f156e5f6bee15d6033be4111
Anna Razafindramiarina <anna@maki-agency.com>
2022-05-12 12:11:56 +0300

    Corrections affichages documents => ajout fichiers sections

---
+++ parts/single/sections/section-document-pdf.php
@@ -0,0 +1,14 @@
+<div class="item">
+<?php if($curr_userdata->ID == $titulaire_id) :?>
+    <input type="checkbox" name="doc_files" class="document document-check" id="pdf-<?= $doc['id'] ?>" value="<?= $doc['id'] ?>">
+    <label for="pdf-<?= $doc['id'] ?>">
+    <div class="ico"><img src="<?= IMG_URL ?>pdf.png" alt="Kotikota"></div>
+    <div class="txt"><?= $doc['name'] ?></div>
+    </label>
+<?php else: ?>
+    <a href="<?= $doc['url'] ?>" class="doc-item-link" target="_blank">
+        <div class="ico"><img src="<?= IMG_URL ?>pdf.png" alt="Kotikota"></div>
+        <div class="txt"><?= $doc['name'] ?></div>
+    </a>
+<?php endif; ?>
+</div>
