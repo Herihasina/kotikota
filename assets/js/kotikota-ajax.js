805fa208ec84639684286e76f3885fa526bfa227
Anna Razafindramiarina <anna@maki-agency.com>
2022-05-16 10:46:01 +0300

    Réglages bug ajout image/video : debug

--- assets/js/kotikota-ajax.js
+++ assets/js/kotikota-ajax.js
@@ -966,6 +966,7 @@ $(function(){
             multiple: false
         });
         mediaUploader.on('select', function() {
+			console.log("doc upload");
             var attachment = mediaUploader.state().get('selection').first().toJSON();
 			$.ajax({
                 url: ajaxurl,
@@ -1010,6 +1011,7 @@ $(function(){
             multiple: false
         });
         mediaUploaderImage.on('select', function() {
+			console.log("image upload");
             var attachment = mediaUploaderImage.state().get('selection').first().toJSON();
             $.ajax({
                 url: ajaxurl,
