38277850e580f26269f46602bb8788e697b42bfb
onyrf82 <onyrf82@gmail.com>
2022-05-19 13:16:25 +0300

    query list triage

--- page-lister_cagnotte.php
+++ page-lister_cagnotte.php
@@ -49,6 +49,37 @@
                 AND ((wp_posts.post_status = "publish"))
             GROUP BY wp_posts.ID ORDER BY CONVERT(count_part,SIGNED INTEGER) DESC';
 
+
+
+            $sql = 'SELECT SQL_CALC_FOUND_ROWS wp_posts.ID
+            FROM ((wp_posts
+            INNER JOIN wp_postmeta mp3 ON (wp_posts.ID = mp3.post_id))
+            INNER JOIN
+            (SELECT mp2.post_id,SUBSTRING_INDEX(SUBSTRING_INDEX(meta_value, ":", 2), ":", -1) AS count_part
+            FROM wp_postmeta mp2
+            WHERE mp2.meta_key = "tous_les_participants") Subquery
+
+            ON (wp_posts.ID = Subquery.post_id))
+            INNER JOIN wp_postmeta mp1 ON (wp_posts.ID = mp1.post_id)
+            WHERE ( (mp1.meta_key = "visibilite_cagnotte" AND mp1.meta_value = "publique")
+            AND ( mp3.meta_key = "cagnotte_cloturee" AND mp3.meta_value = "non"))
+            GROUP BY wp_posts.ID ORDER BY CONVERT(count_part,SIGNED INTEGER) DESC
+            UNION ALL
+            SELECT wp_posts.ID
+            FROM ((wp_posts
+            INNER JOIN wp_postmeta mp1 ON (wp_posts.ID = mp1.post_id))
+            INNER JOIN wp_postmeta mp3 ON (wp_posts.ID = mp3.post_id))
+            INNER JOIN
+            (SELECT mp2.post_id,SUBSTRING_INDEX(SUBSTRING_INDEX(meta_value, ":", 2), ":", -1)
+                  AS count_part
+            FROM wp_postmeta mp2
+            WHERE mp2.meta_key = "tous_les_participants") Subquery
+            ON (wp_posts.ID = Subquery.post_id)
+            WHERE ( Subquery.count_part > '0'
+            AND ( (mp1.meta_key = "visibilite_cagnotte" AND mp1.meta_value = "publique")
+            AND ( mp3.meta_key = "cagnotte_cloturee" AND mp3.meta_value = "oui")))
+            GROUP BY wp_posts.ID ORDER BY CONVERT(count_part,SIGNED INTEGER) DESC';
+
             //query the posts with pagination
             $query_limit = $sql . " LIMIT ".$offset.", ".$per_page."; ";
 
