ed2895329873c9d48ad7b46977f14719b76056af
onyrf82 <onyrf82@gmail.com>
2022-05-19 13:17:42 +0300

    query error

--- page-lister_cagnotte.php
+++ page-lister_cagnotte.php
@@ -58,7 +58,6 @@
             (SELECT mp2.post_id,SUBSTRING_INDEX(SUBSTRING_INDEX(meta_value, ":", 2), ":", -1) AS count_part
             FROM wp_postmeta mp2
             WHERE mp2.meta_key = "tous_les_participants") Subquery
-
             ON (wp_posts.ID = Subquery.post_id))
             INNER JOIN wp_postmeta mp1 ON (wp_posts.ID = mp1.post_id)
             WHERE ( (mp1.meta_key = "visibilite_cagnotte" AND mp1.meta_value = "publique")
@@ -75,7 +74,7 @@
             FROM wp_postmeta mp2
             WHERE mp2.meta_key = "tous_les_participants") Subquery
             ON (wp_posts.ID = Subquery.post_id)
-            WHERE ( Subquery.count_part > '0'
+            WHERE ( Subquery.count_part > "0"
             AND ( (mp1.meta_key = "visibilite_cagnotte" AND mp1.meta_value = "publique")
             AND ( mp3.meta_key = "cagnotte_cloturee" AND mp3.meta_value = "oui")))
             GROUP BY wp_posts.ID ORDER BY CONVERT(count_part,SIGNED INTEGER) DESC';
