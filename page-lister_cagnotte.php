e254e6b545e05eeae951d6a0cf63887e657130c0
onyrf82 <onyrf82@gmail.com>
2022-05-19 15:15:35 +0300

    review query

--- page-lister_cagnotte.php
+++ page-lister_cagnotte.php
@@ -42,14 +42,14 @@
             $sql = 'SELECT SQL_CALC_FOUND_ROWS ID, count_part
                 FROM
                 (SELECT wp_posts.ID, count_part, "id1" OrderKey
-            FROM (($wpdb->posts wp_posts
-            INNER JOIN $wpdb->postmeta mp3 ON (wp_posts.ID = mp3.post_id))
+            FROM (('.$wpdb->posts.' wp_posts
+            INNER JOIN '.$wpdb->postmeta.' mp3 ON (wp_posts.ID = mp3.post_id))
             INNER JOIN
             (SELECT mp2.post_id,SUBSTRING_INDEX(SUBSTRING_INDEX(meta_value, ":", 2), ":", -1) AS count_part
-            FROM $wpdb->postmeta mp2
+            FROM '.$wpdb->postmeta.' mp2
             WHERE mp2.meta_key = "tous_les_participants") Subquery
             ON (wp_posts.ID = Subquery.post_id))
-            INNER JOIN $wpdb->postmeta mp1 ON (wp_posts.ID = mp1.post_id)
+            INNER JOIN '.$wpdb->postmeta.' mp1 ON (wp_posts.ID = mp1.post_id)
             WHERE 1=1
             AND ( (mp1.meta_key = "visibilite_cagnotte" AND mp1.meta_value = "publique")
             AND ( mp3.meta_key = "cagnotte_cloturee" AND mp3.meta_value = "non"))
@@ -58,13 +58,13 @@
             GROUP BY wp_posts.ID
             UNION ALL
             SELECT wp_posts.ID, count_part, "id2" OrderKey
-            FROM (($wpdb->posts wp_posts
-            INNER JOIN $wpdb->postmeta mp1 ON (wp_posts.ID = mp1.post_id))
-            INNER JOIN $wpdb->postmeta mp3 ON (wp_posts.ID = mp3.post_id))
+            FROM (('.$wpdb->posts.' wp_posts
+            INNER JOIN '.$wpdb->postmeta.' mp1 ON (wp_posts.ID = mp1.post_id))
+            INNER JOIN '.$wpdb->postmeta.' mp3 ON (wp_posts.ID = mp3.post_id))
             INNER JOIN
             (SELECT mp2.post_id,SUBSTRING_INDEX(SUBSTRING_INDEX(meta_value, ":", 2), ":", -1)
                   AS count_part
-            FROM $wpdb->postmeta mp2
+            FROM '.$wpdb->postmeta.' mp2
             WHERE mp2.meta_key = "tous_les_participants") Subquery
             ON (wp_posts.ID = Subquery.post_id)
             WHERE 1=1
