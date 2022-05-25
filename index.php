4831dc139fe5c5bf26bd440d1d5cc62351ee663a
arson Rakotomandimby <arson.rakotomandimby@gmail.com>
2022-05-25 10:37:44 +0300

    popup logo

--- index.php
+++ index.php
@@ -35,6 +35,7 @@ get_header(); ?>
         <div class="d-flex">
             <div class="content">
                 <span class="close close-fancy"></span>
+                <span class="logo-pop"><img  src="<?= IMG_URL ?>logo.png"/></span>
                 <div> Ajouter cette application web sur l'ecran d'accueil</div>
                 <div><span>Appuyez sur</span> <span class="img" ><img src="<?= IMG_URL ?>safari-share.jpg"/></span></div>
                 <div>sélectionnez <b>sur l'écran d'accueil</b></div>
