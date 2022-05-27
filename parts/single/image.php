5f41b076b7615e92ff1420f225cf8f88bc3eed2f
onyrf82 <onyrf82@gmail.com>
2022-05-27 15:44:09 +0300

    image cagnotte

--- parts/single/image.php
+++ parts/single/image.php
@@ -1,9 +1,12 @@
 <div class="blc-img">
-    <?php 
+    <?php
       $img_url = attachment_url_to_postid(get_field('illustration_pour_la_cagnotte'));
       if ( $img_url )
-        echo wp_get_attachment_image( $img_url, 'banniere-single' );
-      
+         //echo wp_get_attachment_image( $img_url, 'banniere-single' );
+      ?>
+        <img class="attachment-banniere-single size-banniere-single" src="<?= $img_url ?>">
+      <?php
+
         $limited = get_field('fixer_un_objectif');
         $azo = (int)get_field('montant_recolte');
         $ilaina = (int)get_field('objectif_montant');
@@ -18,8 +21,8 @@
           <span class="ico-percent percent-ok">
             <img src="<?php echo IMG_URL ?>ok.png">
           </span>
-    <?php 
-        }elseif ( $statu < 100 && $limited ){ ?>          
+    <?php
+        }elseif ( $statu < 100 && $limited ){ ?>
           <span class="ico-percent">
               <div class="percent" style="width:45px;height:45px;">
                 <p style="display:none;"><?php echo $statu ?>%</p>
