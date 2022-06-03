e7747abf7a80a93bf2b84766ee9ac89d2bd29790
Anna Razafindramiarina <anna@maki-agency.com>
2022-06-03 12:24:16 +0300

    #2156: permettre sélection multiple de fichier => migration de données == TEST migration rib cagnotte

--- migrate-fields-bo.php
+++ migrate-fields-bo.php
@@ -18,9 +18,14 @@
     $cagnottes= query_posts( $args );
     if($cagnottes && have_posts(  )){
         foreach ( $cagnottes as $cagnotte){
-            $RIB_fichier = get_field('rib_fichier', $cagnotte->ID);
-            if($RIB_fichier)
-                dump($RIB_fichier);
+            $rib_file_id = get_field('rib_fichier', $cagnotte->ID);
+            if($rib_file_id){
+                echo "cagnotte id: ".$cagnotte->ID;
+                dump($rib_file_id);
+                $rib_file = attachment_url_to_postid(strip_tags($rib_file_id));
+                $add_rib_file = add_row('fichiers_rib',array('fichier' => $rib_file),$cagnotte->ID);
+                dump($add_rib_file);
+            }
 
         }
     }
